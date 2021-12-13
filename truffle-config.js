939e944de93e2908551678cfb8fecc72ffbfdc2a
clau <claudiabarcelovaldes40@gmail.com>
2021-12-13 16:12:09 +0100

    update truffle conf

--- truffle-config.js
+++ truffle-config.js
@@ -57,7 +57,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "^0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.6",    // Fetch exact version from solc-bin (default: truffle's version)
       optimizer:{
         enabled: false,
          runs: 200
